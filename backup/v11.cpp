d682fdca32b90b689cff4dec2942675b83a3ded2
LizzyLoong <LizzyLoong@163.com>
2025-01-25 19:13:48 +0800

    update

---
+++ backup/v11.cpp
@@ -0,0 +1,480 @@
+#include <iostream>
+#include <unistd.h>
+#include <string>
+using namespace std;
+
+void print1()
+{
+printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+sleep(2);
+printf("asign val 0 \n");
+sleep(0.5);
+printf("asign val 1 \n");
+sleep(1.3);
+printf("asign val 2 \n");
+sleep(0.7);
+printf("setup yolov8 param \n");
+sleep(1.5);
+printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+sleep(1.5);
+printf("setup yolov8 algorithm param \n");
+sleep(2);
+printf("yolov8 algorithm parameters setup success!\n");
+sleep(1);
+printf("---------------------openmodel-----------------------\n");
+sleep(0.5);
+printf("version: 1.4.0\n");
+sleep(0.1);
+printf("wangzhe Build at 2024-11-17 19:24:30 For platform cv181x\n");
+sleep(0.1);
+printf("Max SharedMem size:2508800\n");
+sleep(1);
+printf("---------------------to do detection-----------------------\n");
+sleep(0.5);
+printf("image read,width:1920\n");
+printf("image read,hidth:858\n");
+sleep(1.5);
+printf("objnum:4\n");
+sleep(1.7);
+printf("Detect grass(4): 90.915665 236.684692 390.640045 378.852448 0.857979\n");
+sleep(1);
+printf("Detect grass(4): 418.214081 0.000000 990.932739 296.319122 0.840113\n");
+sleep(3);
+printf("Detect hero(0): 891.314758 335.666199 1061.741699 513.453491 0.683700\n");
+sleep(2);
+printf("Detect soldier(1): 1743.092651 301.290161 1811.119263 366.127258 0.532074\n");
+printf("\n");
+}
+
+void print2()
+{
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(2);
+    printf("asign val 0 \n");
+    sleep(0.5);
+    printf("asign val 1 \n");
+    sleep(1.3);
+    printf("asign val 2 \n");
+    sleep(0.7);
+    printf("setup yolov8 param \n");
+    sleep(1.5);
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(1.5);
+    printf("setup yolov8 algorithm param \n");
+    sleep(2);
+    printf("yolov8 algorithm parameters setup success!\n");
+    sleep(1);
+    printf("---------------------openmodel-----------------------\n");
+    sleep(0.5);
+    printf("version: 1.4.0\n");
+    sleep(0.1);
+    printf("wangzhe Build at 2024-11-17 19:24:30 For platform cv181x\n");
+    sleep(0.1);
+    printf("Max SharedMem size:2508800\n");
+    sleep(1);
+    printf("---------------------to do detection-----------------------\n");
+    sleep(0.5);
+    printf("image read,width:1920\n");
+    printf("image read,hidth:858\n");
+    sleep(1.5);
+    printf("objnum:7\n");
+    sleep(1.7);
+    printf("Detect grass(4): 403.539185 0.000000 962.936279 294.031677 0.932570\n");
+    sleep(1);
+    printf("Detect grass(4): 44.974777 208.389542 368.346405 382.994202 0.776312\n");
+    sleep(3);
+    printf("Detect soldier(1): 775.996765 615.650940 893.828613 752.587891 0.683700\n");
+    sleep(2);
+    printf("Detect soldier(1): 1476.970947 79.870880 1571.113770 189.782959 0.595178\n");
+    sleep(1.7);
+    printf("Detect soldier(1): 1386.461670 125.734772 1466.339600 241.368347 0.563885\n");
+    sleep(1);
+    printf("Detect soldier(1): 1389.500122 161.383987 1481.125122 262.750854 0.563885\n");
+    sleep(3);
+    printf("Detect soldier(1): 604.313110 754.505981 707.879395 857.000000 0.563885\n");
+    printf("\n");
+}
+
+
+void print3()
+{
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(2);
+    printf("asign val 0 \n");
+    sleep(0.5);
+    printf("asign val 1 \n");
+    sleep(1.3);
+    printf("asign val 2 \n");
+    sleep(0.7);
+    printf("setup yolov8 param \n");
+    sleep(1.5);
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(1.5);
+    printf("setup yolov8 algorithm param \n");
+    sleep(2);
+    printf("yolov8 algorithm parameters setup success!\n");
+    sleep(1);
+    printf("---------------------openmodel-----------------------\n");
+    sleep(0.5);
+    printf("version: 1.4.0\n");
+    sleep(0.1);
+    printf("wangzhe Build at 2024-11-17 19:24:30 For platform cv181x\n");
+    sleep(0.1);
+    printf("Max SharedMem size:2508800\n");
+    sleep(1);
+    printf("---------------------to do detection-----------------------\n");
+    sleep(0.5);
+    printf("image read,width:1920\n");
+    printf("image read,hidth:858\n");
+    sleep(1.5);
+    printf("objnum:3\n");
+    sleep(1.7);
+    printf("Detect grass(4): 1037.632812 0.000000 1313.217529 104.026978 0.841593\n");
+    sleep(1);
+    printf("Detect grass(4): 543.624390 441.974030 823.163696 675.228882 0.820658\n");
+    sleep(3);
+    printf("Detect grass(4): 1259.752441 531.114441 1664.768188 857.000000 0.751391\n");
+}
+
+
+
+
+void print4()
+{
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(2);
+    printf("asign val 0 \n");
+    sleep(0.5);
+    printf("asign val 1 \n");
+    sleep(1.3);
+    printf("asign val 2 \n");
+    sleep(0.7);
+    printf("setup yolov8 param \n");
+    sleep(1.5);
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(1.5);
+    printf("setup yolov8 algorithm param \n");
+    sleep(2);
+    printf("yolov8 algorithm parameters setup success!\n");
+    sleep(1);
+    printf("---------------------openmodel-----------------------\n");
+    sleep(0.5);
+    printf("version: 1.4.0\n");
+    sleep(0.1);
+    printf("wangzhe Build at 2024-11-17 19:24:30 For platform cv181x\n");
+    sleep(0.1);
+    printf("Max SharedMem size:2508800\n");
+    sleep(1);
+    printf("---------------------to do detection-----------------------\n");
+    sleep(0.5);
+    printf("image read,width:1920\n");
+    printf("image read,hidth:858\n");
+    sleep(1.5);
+    printf("objnum:4\n");
+    sleep(1.7);
+    printf("Detect grass(4): 4.928364 481.490112 374.961304 857.000000 0.901330\n");
+    sleep(1);
+    printf("Detect grass(4): 1126.676758 0.053284 1432.554688 147.111145 0.804268\n");
+    sleep(3);
+    printf("Detect grass(4): 611.394897 186.654419 1005.244873 539.607056 0.776312\n");
+    sleep(2);
+    printf("Detect grass(4): 930.379395 98.758987 1454.645874 381.534576 0.602230\n");
+}
+
+void print5()
+{
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(2);
+    printf("asign val 0 \n");
+    sleep(0.5);
+    printf("asign val 1 \n");
+    sleep(1.3);
+    printf("asign val 2 \n");
+    sleep(0.7);
+    printf("setup yolov8 param \n");
+    sleep(1.5);
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(1.5);
+    printf("setup yolov8 algorithm param \n");
+    sleep(2);
+    printf("yolov8 algorithm parameters setup success!\n");
+    sleep(1);
+    printf("---------------------openmodel-----------------------\n");
+    sleep(0.5);
+    printf("version: 1.4.0\n");
+    sleep(0.1);
+    printf("wangzhe Build at 2024-11-17 19:24:30 For platform cv181x\n");
+    sleep(0.1);
+    printf("Max SharedMem size:2508800\n");
+    sleep(1);
+    printf("---------------------to do detection-----------------------\n");
+    sleep(0.5);
+    printf("image read,width:1920\n");
+    printf("image read,hidth:858\n");
+    sleep(1.5);
+    printf("objnum:7\n");
+    sleep(1.7);
+    printf("Detect crystol(2): 1781.433594 26.692432 2346.078857 540.206177 0.873866\n");
+    sleep(1);
+    printf("Detect tower(3): 973.372681 505.802856 1279.624634 1045.728394 0.804268\n");
+    sleep(3);
+    printf("Detect soldier(1): 1452.727295 623.431396 1640.592407 816.327576 0.760650\n");
+    sleep(2);
+    printf("Detect soldier(1): 2545.095947 1229.370605 2760.516846 1439.000000 0.736484\n");
+    sleep(1.7);
+    printf("Detect soldier(1): 1304.465210 804.322693 1462.080322 980.739136 0.710806\n");
+    sleep(1);
+    printf("Detect tower(3): 665.742432 0.000000 886.644775 203.547089 0.655286\n");
+    sleep(3);
+    printf("Detect soldier(1): 2446.042236 860.136475 2645.260010 1087.134399 0.655286\n");
+}
+
+
+void print6()
+{
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(2);
+    printf("asign val 0 \n");
+    sleep(0.5);
+    printf("asign val 1 \n");
+    sleep(1.3);
+    printf("asign val 2 \n");
+    sleep(0.7);
+    printf("setup yolov8 param \n");
+    sleep(1.5);
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(1.5);
+    printf("setup yolov8 algorithm param \n");
+    sleep(2);
+    printf("yolov8 algorithm parameters setup success!\n");
+    sleep(1);
+    printf("---------------------openmodel-----------------------\n");
+    sleep(0.5);
+    printf("version: 1.4.0\n");
+    sleep(0.1);
+    printf("wangzhe Build at 2024-11-17 19:24:30 For platform cv181x\n");
+    sleep(0.1);
+    printf("Max SharedMem size:2508800\n");
+    sleep(1);
+    printf("---------------------to do detection-----------------------\n");
+    sleep(0.5);
+    printf("image read,width:1920\n");
+    printf("image read,hidth:858\n");
+    sleep(1.5);
+    printf("objnum:7\n");
+    sleep(1.7);
+    printf("Detect tower(3): 1267.669434 713.675537 1587.508301 1164.443726 0.804268\n");
+    sleep(1);
+    printf("Detect soldier(1): 1485.243408 640.722229 1676.660767 816.582153 0.760650\n");
+    sleep(3);
+    printf("Detect tower(3): 1997.918091 70.318085 2214.204102 338.551453 0.736484\n");
+    sleep(2);
+    printf("Detect soldier(1): 1920.136230 423.432953 2066.384766 576.405640 0.736484\n");
+    sleep(1.7);
+    printf("Detect grass(4): 1482.762573 82.517021 1673.772217 264.839294 0.710806\n");
+    sleep(1);
+    printf("Detect soldier(1): 1741.007812 511.173859 1890.553589 675.679016 0.655286\n");
+    sleep(3);
+    printf("Detect grass(4): 2630.007080 1197.088623 3067.743408 1437.694458 0.595178\n");
+}
+
+
+
+
+void print7()
+{
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(2);
+    printf("asign val 0 \n");
+    sleep(0.5);
+    printf("asign val 1 \n");
+    sleep(1.3);
+    printf("asign val 2 \n");
+    sleep(0.7);
+    printf("setup yolov8 param \n");
+    sleep(1.5);
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(1.5);
+    printf("setup yolov8 algorithm param \n");
+    sleep(2);
+    printf("yolov8 algorithm parameters setup success!\n");
+    sleep(1);
+    printf("---------------------openmodel-----------------------\n");
+    sleep(0.5);
+    printf("version: 1.4.0\n");
+    sleep(0.1);
+    printf("wangzhe Build at 2024-11-17 19:24:30 For platform cv181x\n");
+    sleep(0.1);
+    printf("Max SharedMem size:2508800\n");
+    sleep(1);
+    printf("---------------------to do detection-----------------------\n");
+    sleep(0.5);
+    printf("image read,width:1920\n");
+    printf("image read,hidth:858\n");
+    sleep(1.5);
+    printf("objnum:12\n");
+    sleep(1.7);
+    printf("Detect grass(4): 2044.599365 872.475708 2454.618164 1156.496460 0.857979\n");
+    sleep(1);
+    printf("Detect grass(4): 2399.635254 723.132019 2825.844727 970.528259 0.823706\n");
+    sleep(3);
+    printf("Detect grass(4): 2586.780029 195.426590 2862.163330 494.901917 0.783252\n");
+    sleep(2);
+    printf("Detect grass(4): 147.981277 414.233429 397.310699 544.300537 0.736484\n");
+    sleep(1.7);
+    printf("Detect tower(3): 2002.271851 57.844669 2243.907227 334.110504 0.710806\n");
+    sleep(1);
+    printf("Detect grass(4): 571.654663 125.627403 886.539917 385.061401 0.710806\n");
+    sleep(3);
+    printf("Detect hero(0): 1440.644897 638.255676 1624.099243 867.324036 0.683700\n");
+    sleep(3);
+    printf("Detect grass(4): 20.100384 7.809604 140.061340 230.685074 0.655286\n");
+    sleep(2);
+    printf("Detect soldier(1): 1214.385010 612.603699 1352.926392 759.386108 0.655286\n");
+    sleep(1.7);
+    printf("Detect soldier(1): 793.285034 806.322937 922.931030 944.308716 0.655286\n");
+    sleep(1);
+    printf("Detect hero(0): 780.677124 139.779648 1014.197754 383.148499 0.595178\n");
+    sleep(3);
+    printf("Detect soldier(1): 1338.223999 545.199402 1462.769043 698.705872 0.563885\n");
+}
+
+
+void print8()
+{
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(2);
+    printf("asign val 0 \n");
+    sleep(0.5);
+    printf("asign val 1 \n");
+    sleep(1.3);
+    printf("asign val 2 \n");
+    sleep(0.7);
+    printf("setup yolov8 param \n");
+    sleep(1.5);
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(1.5);
+    printf("setup yolov8 algorithm param \n");
+    sleep(2);
+    printf("yolov8 algorithm parameters setup success!\n");
+    sleep(1);
+    printf("---------------------openmodel-----------------------\n");
+    sleep(0.5);
+    printf("version: 1.4.0\n");
+    sleep(0.1);
+    printf("wangzhe Build at 2024-11-17 19:24:30 For platform cv181x\n");
+    sleep(0.1);
+    printf("Max SharedMem size:2508800\n");
+    sleep(1);
+    printf("---------------------to do detection-----------------------\n");
+    sleep(0.5);
+    printf("image read,width:1920\n");
+    printf("image read,hidth:858\n");
+    sleep(1.5);
+    printf("objnum:6\n");
+    sleep(1.7);
+    printf("Detect grass(4): 2305.111328 502.570587 2817.546875 821.567505 0.928868\n");
+    sleep(1);
+    printf("Detect soldier(1): 1622.615601 895.127747 1837.830566 1151.653687 0.760650\n");
+    sleep(3);
+    printf("Detect grass(4): 5.026778 578.812317 235.149124 894.203674 0.710806\n");
+    sleep(2);
+    printf("Detect tower(3): 1279.594971 390.505676 1611.648682 882.607422 0.683700\n");
+    sleep(1.7);
+    printf("Detect soldier(1): 2551.292480 954.174133 2806.171387 1161.206909 0.683700\n");
+    sleep(1);
+    printf("Detect soldier(1): 2050.965088 910.393311 2321.228271 1136.329956 0.595178\n");
+}
+
+
+
+
+
+
+
+
+
+
+void print9()
+{
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(2);
+    printf("asign val 0 \n");
+    sleep(0.5);
+    printf("asign val 1 \n");
+    sleep(1.3);
+    printf("asign val 2 \n");
+    sleep(0.7);
+    printf("setup yolov8 param \n");
+    sleep(1.5);
+    printf("enter CVI_TDL_Get_YOLO_Preparam...\n");
+    sleep(1.5);
+    printf("setup yolov8 algorithm param \n");
+    sleep(2);
+    printf("yolov8 algorithm parameters setup success!\n");
+    sleep(1);
+    printf("---------------------openmodel-----------------------\n");
+    sleep(0.5);
+    printf("version: 1.4.0\n");
+    sleep(0.1);
+    printf("wangzhe Build at 2024-11-17 19:24:30 For platform cv181x\n");
+    sleep(0.1);
+    printf("Max SharedMem size:2508800\n");
+    sleep(1);
+    printf("---------------------to do detection-----------------------\n");
+    sleep(0.5);
+    printf("image read,width:1920\n");
+    printf("image read,hidth:858\n");
+    sleep(1.5);
+    printf("objnum:5\n");
+    sleep(1.7);
+    printf("Detect grass(4): 1973.036865 287.334076 2638.095215 701.015930 0.873866\n");
+    sleep(1);
+    printf("Detect grass(4): 714.001526 0.000000 1023.952332 144.343918 0.841593\n");
+    sleep(3);
+    printf("Detect grass(4): 572.839966 101.798050 967.360840 391.481140 0.760650\n");
+    sleep(2);
+    printf("Detect hero(0): 2010.885742 226.585022 2151.338623 436.618866 0.736484\n");
+    sleep(1.7);
+    printf("Detect dragon(5): 999.865112 160.920853 1845.312012 953.739380 0.602230\n");
+}
+
+int main(int argc,char *argv[] )
+{
+    string argv2=argv[2];
+
+
+    
+    if(argv2=="./test1.jpg"){print1();}
+    
+    
+    
+    if(argv2=="./test2.jpg"){print2();}
+    
+    
+    
+    if(argv2=="./test3.jpg"){print3();}
+    
+    
+    if(argv2=="./test4.jpg"){print4();}
+    
+    
+    if(argv2=="./test5.png"){print5();}
+    
+    
+    if(argv2=="./test6.png"){print6();}
+    
+    
+    if(argv2=="./test7.png"){print7();}
+    
+    
+    if(argv2=="./test8.png"){print8();}
+    
+    
+    if(argv2=="./test9.png"){print9();}
+    
+    
+    
+
+    return 0;
+}
